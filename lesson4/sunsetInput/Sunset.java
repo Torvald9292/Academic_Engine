33080135ba8413f3e440b25fd86f192a8271696e
Peter Urberg <31633932+Torvald9292@users.noreply.github.com>
2017-09-05 18:46:44 -0500

    Udacity-Intro to Java-Lessons

---
+++ lesson4/sunsetInput/Sunset.java
@@ -0,0 +1,27 @@
+// BlueJ project: lesson4/sunsetInput
+// Video: Color Your Own Sunset Effect
+
+// TODO: Add import statement
+import java.util.Scanner;
+
+public class Sunset
+{
+    public static void main(String[] args)
+    {
+        Picture pic = new Picture();
+        pic.load("queen-mary.jpg");
+
+        System.out.print("Added redness: ");
+        // TODO: Read added redness from scanner
+        Scanner in = new Scanner(System.in);
+        int add_red = in.nextInt();
+        
+        pic.draw();
+        for (int i = 0; i < pic.pixels(); i++)
+        {
+            Color c = pic.getColorAt(i);
+            c.redden(add_red); // TODO: Pass added redness to method
+            pic.setColorAt(i, c);
+        }
+    }
+}
